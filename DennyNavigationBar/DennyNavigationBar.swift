c0300237d5711d07d4a40fbeacac5144c345c7eb
denny.k <denny.k@kakaocorp.com>
2020-09-06 12:54:21 +0900

    Method to set Bar Bottom Border Hidden

--- DennyNavigationBar/DennyNavigationBar.swift
+++ DennyNavigationBar/DennyNavigationBar.swift
@@ -11,13 +11,27 @@ import UIKit
 public class DennyNavigationBar: UINavigationBar {
     private let emptyShadowImage: UIImage? = UIImage(color: UIColor.clear, size: CGSize(width: 1, height: 1.0 / UIScreen.main.scale))
     
+    private var bottomBorderImage: UIImage? {
+        didSet {
+            self.shadowImage = bottomBorderImage
+        }
+    }
+    
     public func setBackButtonImage(image: UIImage?) {
         self.backIndicatorImage = image
         self.backIndicatorTransitionMaskImage = image
     }
     
+    public func setBottomBorderHidden(isHidden: Bool) {
+        if isHidden {
+            self.shadowImage = emptyShadowImage
+        } else {
+            self.shadowImage = bottomBorderImage
+        }
+    }
+    
     public func removeBottomBorder() {
-        self.shadowImage = emptyShadowImage
+        self.bottomBorderImage = emptyShadowImage
     }
     
     public func setButtonTintColor(color: UIColor) {
@@ -30,6 +44,6 @@ public class DennyNavigationBar: UINavigationBar {
     }
     
     public func setBottomBorderColor(color: UIColor, height: CGFloat = (1.0 / UIScreen.main.scale)) {
-        self.shadowImage = UIImage.init(color: color, size: CGSize(width: height, height: height))
+        self.bottomBorderImage = UIImage.init(color: color, size: CGSize(width: height, height: height))
     }
 }
